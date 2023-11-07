878592df2f873bdec0eb5e0fc923fc28e5864377
Enzo1987Pavel <enzo1987@mail.ru>
2023-11-07 14:49:33 +0300

    init commit1

--- main.py
+++ main.py
@@ -8,10 +8,5 @@ def main_page():
     return render_template("index.html", name="index")
 
 
-@app.route("/<first_name>/")
-def main_page_post():
-    return render_template("index_post.html", name="index_post")
-
-
 if __name__ == "__main__":
     app.run()
